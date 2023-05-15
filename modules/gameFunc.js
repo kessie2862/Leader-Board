72e7129f52c80db7288d2c767d1ace2cda815756
kessie <kayp3477@gmail.com>
2023-05-15 15:09:17 +0000

    Add comments

--- modules/gameFunc.js
+++ modules/gameFunc.js
@@ -1,21 +1,22 @@
+// 1. Create a new score item for the list
+// 2. Get all scores for a game from the LeaderBoard API
+// 3. Save score for a game to the API
+
 const apiUrl =
   'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
-// create a new score item for the list
 export const createGameScores = (name, score) => {
   const li = document.createElement('li');
   li.textContent = `${name}: ${score}`;
   return li;
 };
 
-// get all scores for a game from the API
 export const getGameScores = async (gameId) => {
   const response = await fetch(`${apiUrl}games/${gameId}/scores/`);
   const data = await response.json();
   return data.result;
 };
 
-// save score for a game to the API
 export const saveGameScore = async (gameId, name, score) => {
   const response = await fetch(`${apiUrl}games/${gameId}/scores/`, {
     method: 'POST',
